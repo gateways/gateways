af787954f7bf12bd8e07d9c5e6c4672e1235bf43
Aaron Comyn <aaron@roxor.no>
2015-11-18 11:35:17 +0100

    Tweaks to prep for population

--- gateway-hub.fsx
+++ gateway-hub.fsx
@@ -6,6 +6,7 @@
 
 *)
 
+
 open System
 open System.IO
 
@@ -14,31 +15,32 @@ let relpath path = Path.Combine(__SOURCE_DIRECTORY__, path)
 
 type FilePath = string
 
-type INamed = 
-  abstract Name : string
+type IHaveAPath = abstract path : string
 
 type gateway = 
     { name : string }
-    interface INamed with
-        member x.Name = x.name
+    interface IHaveAPath with
+        member x.path = x.name
 
 type gatewayroot = 
     { name : string 
+      root : FilePath
       assets : FilePath list 
       antiassets : FilePath list }
-    interface INamed with
-        member x.Name = x.name
+    interface IHaveAPath with
+        member x.path = x.root
 
 let gateways = 
     Directory.GetDirectories(relpath "") 
     |> Seq.map Path.GetFileNameWithoutExtension 
-    |> Seq.where (fun g -> not (String.IsNullOrWhiteSpace(g) || g.StartsWith(".") || g.StartsWith("_") || g = "common" ))  
+    |> Seq.where (fun g -> not (String.IsNullOrWhiteSpace(g) || g.StartsWith(".") || g.StartsWith("_") ))  
     |> Seq.map (fun g -> { gateway.name = g } )
     |> Seq.toList
 
 
-let common = 
-    { name = "common"
+let shared = 
+    { name = "shared"
+      root = "_shared"
       assets = 
         [ "_layouts/"
           "_layouts/default.html" 
@@ -50,18 +52,17 @@ let common =
           "params.json" ] 
       antiassets = [ "index.html" ] }
 
-let pathto (source:'a when 'a :> INamed) asset =
-    relpath source.Name + "/" + asset
+let pathto (source:'a when 'a :> IHaveAPath) asset =
+    relpath source.path + "/" + asset
 
 let commonpath name = relpath "common/" + name
 let gatewaypath gateway name = relpath
 
-
 let populate =
     for g in gateways do
-        for ass in common.assets do
-            File.Copy((pathto common ass), (pathto g ass))
-        for antiass in common.antiassets do
+        for ass in shared.assets do
+            File.Copy((pathto shared ass), (pathto g ass))
+        for antiass in shared.antiassets do
             File.Delete(pathto g antiass)
 
         printf "%s" g.name
