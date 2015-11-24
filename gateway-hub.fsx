ba6386a11fac049619bd0d28456e0f89fb20510b
Aaron Comyn <aaron@roxor.no>
2015-11-24 20:38:57 +0100

    Filled out domain templates

--- gateway-hub.fsx
+++ gateway-hub.fsx
@@ -102,32 +102,114 @@ module Gateways =
 
     module internal Sites =
 
-        let Eutro = { EmptyGateway with Title = "Eutro" }
+        let Eutro =
+          { EmptyGateway with
+              Title = "Eutro"
+              Sections = [ "Contact"; "Services"; "Partners"; "Projects" ]
+              Design = "eutro" }
 
-        let iforsk = { EmptyGateway with Title = "iforsk" }
-        let forforskning = { EmptyGateway with Title = "forforskning" }
-        let forresearch = { EmptyGateway with Title = "forresearch" }
+        let iforsk =
+          { EmptyGateway with
+              Title = "iforsk"
+              Sections = [ "Tools"; "Events"; "Courses"; "Resources"; "News"; ]
+              Design = "iforsk" }
 
-        let Helseregistre = { EmptyGateway with Title = "Helseregistre" }
-        let Helsereg = { EmptyGateway with Title = "Helsereg" }
-        let Healthreg = { EmptyGateway with Title = "Healthreg" }
-        let variabler = { EmptyGateway with Title = "variabler" }
-        let variables = { EmptyGateway with Title = "variables" }
+        let forforskning =
+            { EmptyGateway with
+                Title = "forforskning"
+                Sections = [ "Directory"; "Resources"; ]
+                Design = "catalogue" }
 
-        let Kodeverket = { EmptyGateway with Title = "Kodeverket" }
-        let kodebok = { EmptyGateway with Title = "kodebok" }
-        let codebook = { EmptyGateway with Title = "codebook" }
+        let forresearch =
+            { EmptyGateway with
+                Title = "forresearch"
+                Sections = [ "Katalog"; "Ressurs"; ]
+                Design = "catalogue" }
+
+        let Helseregistre =
+            { EmptyGateway with
+                Title = "Helseregistre"
+                Sections = [ "Registre"; "Ressurs"; "Nyheter"; "Eventer" ]
+                Design = "directory" }
+
+        let Helsereg =
+            { EmptyGateway with
+                Title = "Helsereg"
+                Sections = [ "Registre"; "Ressurs"; "Nyheter"; "Eventer" ]
+                Design = "catalogue" }
+
+        let Healthreg =
+            { EmptyGateway with
+                Title = "Healthreg"
+                Sections = [ "Directory"; ]
+                Design = "catalogue" }
+
+        let variabler =
+            { EmptyGateway with
+                Title = "variabler"
+                Sections = [ "Katalog"; "Ressurs"; ]
+                Design = "catalogue" }
+
+        let variables =
+            { EmptyGateway with
+                Title = "variables"
+                Sections = [ "Directories"; "Resources"; ]
+                Design = "catalogue" }
+
+        let Kodeverket =
+            { EmptyGateway with
+                Title = "Kodeverket"
+                Sections = [ "Dugnad"; "Koder"; "Nyheter"; ]
+                Design = "directory" }
+
+        let kodebok =
+            { EmptyGateway with
+                Title = "kodebok"
+                Sections = [ "Katalog"; "Ressurs"; "Dele"; ]
+                Design = "catalogue" }
+
+        let codebook =
+            { EmptyGateway with
+                Title = "codebook"
+                Sections = [ "Directory"; "Resources"; "Share"; ]
+                Design = "catalogue" }
+
         let kvrk =
             { EmptyGateway with
                 Title = "kvrk"
-                Sections = [ "Diagnosis"; "Procedure"; "Prescription" ]  //todo: "news/major" "news/minor" "news/whaaaa"
-                Design = "oppslag" }
+                Sections = [ "Diagnosis"; "Procedure"; "Prescription" ]
+                Design = "directory" }
 
-        let Repository = { EmptyGateway with Title = "Repsitory" }
-        let Taxonomy = { EmptyGateway with Title = "Taxonomy" }
-        let Taksonomi = { EmptyGateway with Title = "Taksonomi" }
-        let Ontologi = { EmptyGateway with Title = "Ontologi" }
-        let Ontology = { EmptyGateway with Title = "Ontology" }
+        let Repository =
+            { EmptyGateway with
+                Title = "Repsitory"
+                Sections = [ "Directory"; ]
+                Design = "catalogue" }
+
+        let Taxonomy =
+            { EmptyGateway with
+                Title = "Taxonomy"
+                Sections = [ "Directory"; ]
+                Design = "catalogue" }
+
+        let Taksonomi =
+            { EmptyGateway with
+                Title = "Taksonomi"
+                Sections = [ "Katalog"; ]
+                Design = "catalogue" }
+
+        let Ontologi =
+            { EmptyGateway with
+                Title = "Ontologi"
+                Sections = [ "Katalog" ]
+                Design = "catalogue" }
+
+        let Ontology =
+            { EmptyGateway with
+                Title = "Ontology"
+                Sections = [ "Directory"; ]
+                Design = "catalogue" }
+
 
         let all =
             [ Eutro
@@ -170,14 +252,14 @@ module ContentCurator =
         printf "updated %s\r\n, repo in %s\r\n" g.root repo
 
 
-    module private StructuralValidation = 
+    module private StructuralValidation =
 
         let ensureDirectory path = Directory.CreateDirectory(path) |> ignore
         let safeCopy from to' = if not (File.Exists(to')) then File.Copy(from, to')
         let sharedroot = relpath shared.root
         let designs = Gateways.sites |> List.groupBy (fun g -> g.Design)
 
-        let ensureLayouts = 
+        let ensureLayouts () =
             let root = sharedroot + "/_layouts/"
             for design, gateways in designs do
                 let designFolder = root + design.ToLower() + "/"
@@ -189,28 +271,32 @@ module ContentCurator =
             let correctedText = File.ReadAllText(source).Replace(@"{%% site.section %%}", section )
             File.WriteAllText(target, correctedText)
 
-        let ensureSections =
-            for site in Gateways.sites do 
+        let ensureSections () =
+            for site in Gateways.sites do
                 let root = relpath site.Title + "/"
                 for section in site.Sections do
-                    let sectionpath = root + section.ToLower() + "/"
-                    ensureDirectory (sectionpath) 
-                    //ensureDirectory (sectionpath + "_posts/") 
 
-                    let sectionIndex = sectionpath +  "index.html"
-                    if not (File.Exists(sectionIndex)) then
-                        pass (sharedroot + @"/section/index.html") sectionIndex section
+                    let sectionPage = root + section.ToLower() + ".md"
+                    if not (File.Exists(sectionPage)) then
+                        pass (sharedroot + @"/section/section.md") sectionPage section
+
+//                    let sectionpath = root + section.ToLower() + "/"
+//                    ensureDirectory (sectionpath)
+//                    //ensureDirectory (sectionpath + "_posts/")
+//
+//                    let sectionIndex = sectionpath +  "index.html"
+//                    if not (File.Exists(sectionIndex)) then
+//                        pass (sharedroot + @"/section/index.html") sectionIndex section
 
                         //https://github.com/raphinou/jekyll-base/blob/gh-pages/_layouts/index.html
     let ensureDesigns () =
-        StructuralValidation.ensureLayouts
-        StructuralValidation.ensureSections
+        StructuralValidation.ensureLayouts ()
+        StructuralValidation.ensureSections ()
 
- 
+
     module internal Asset =
 
         let ensure (g:site, ass:FilePath) =
-
             let cname g ass = File.WriteAllLines((pathto g ass), [ g.root + ".no" ])
             let mkdir g ass = Directory.CreateDirectory(pathto g ass) |> ignore
             let pass  g ass =
@@ -220,28 +306,31 @@ module ContentCurator =
             let (|Cname|_|) (g, ass) = check (ass = "CNAME")
             let (|IsDir|_|) (g, ass) = check ((ass:FilePath).EndsWith("/"))
 
-            function
+            match g, ass with
             | Cname -> cname g ass
             | IsDir -> mkdir g ass
             | _ ->     pass  g ass
 
         let delete (g:site, antiass:FilePath) = File.Delete(pathto g antiass)
 
-    let populategateway (g:site) =
+    let populateSite (s:site) =
 
-        let withgateway assets = assets |> List.map (fun a -> g,a)
-        let delete assets = assets |> withgateway |> List.map Asset.delete |> ignore
-        let ensure assets = assets |> withgateway |> List.map Asset.ensure |> ignore
+
+        let withsite assets = assets |> List.map (fun a -> s,a)
+        let delete assets = assets |> withsite |> List.map Asset.delete |> ignore
+        let ensure assets = assets |> withsite |> List.map Asset.ensure |> ignore
 
         shared.antiassets |> delete
         shared.assets     |> ensure
-        publish g
 
-    let populate () = 
+
+    let populate () =
         ensureDesigns ()
-        gateways |> List.map populategateway
+        gateways |> List.map populateSite
 
-    let publishall () = gateways |> List.map publish
+    let publishAll () = gateways |> List.map publish
 
 
 ContentCurator.ensureDesigns ()
+ContentCurator.populate ()
+ContentCurator.publishAll ()
